d700a676fa0c425be58e3f27f35ad4a71026b0f4
IRobot <flitewka@gmail.com>
2025-01-08 10:24:58 +0100

    Movement kamery góra, dół, wsad za postacią

--- D&D/Assets/Scripts/playerMovement.cs
+++ D&D/Assets/Scripts/playerMovement.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     // Player speed variable, controls velocity multiplier
-    public float playerSpeed = 1f;
-    public float jumpAmount = 1f;
+    public float playerSpeed = 5f;
+    public float rotationY = 0f;
     Rigidbody rb;
 
     // Start is called before the first frame update
@@ -24,9 +24,43 @@ public class playerMovement : MonoBehaviour
         // w key changes value to 1, s key changes value to -1
         float zMove = Input.GetAxisRaw("Vertical");
 
+        // Preserve the current y velocity for falling and jumping
+        float yMove = 0f;
 
-        // Creates velocity in direction of value equal to keypress (WASD)
-        // rb.velocity.y deals with falling + jumping by setting velocity to y.
-        rb.velocity = new Vector3(xMove, rb.velocity.y, zMove) * playerSpeed;
+        // Checking which button is pressed
+        if (Input.GetKey(KeyCode.E))
+        {
+            rotationY += 0.5f;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rotationY -= 0.5f;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            yMove = 1f;
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            yMove = -1f;
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            playerSpeed = 10f;
+        }
+        else
+        {
+            playerSpeed = 5f;
+        }
+
+        // Local move
+        Vector3 localMove = new Vector3(xMove, yMove, zMove);
+        Vector3 worldMove = transform.TransformDirection(localMove);
+
+        // Player movement
+        rb.velocity = worldMove * playerSpeed;
+
+        // Player rotation
+        transform.rotation = Quaternion.Euler(0, rotationY, 0);
     }
 }
