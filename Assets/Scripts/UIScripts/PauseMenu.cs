b8f034007c82e63e63e73519febcc44f1123711e
AlgorithmMstr <flitewka@gmail.com>
2025-06-06 19:14:32 +0200

    UI
    Added UI by clicking ESCAPE

---
+++ Assets/Scripts/UIScripts/PauseMenu.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+
+    private bool isPaused = false;
+
+    void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+        Debug.Log("Exiting game...");
+    }
+
+    public void SaveGame()
+    {
+        Debug.Log("Saving game...");
+    }
+
+    public void LoadGame()
+    {
+        Debug.Log("Loading game...");
+    }
+}
